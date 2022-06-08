af4d4797c868f2c534f946940d60562eccb14430
WrathOfZombies <bhargav.krishna@outlook.com>
2022-06-08 22:08:07 +0530

    feat: improving the ci & cd systems

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,26 @@
 import * as dotenv from "dotenv";
 import * as express from "express";
+import rateLimit from "express-rate-limit";
 import * as functions from "firebase-functions";
+import helmet from "helmet";
 import { createPosts } from "./models/post";
 import { downloadPosts } from "./models/unsplash";
 import routes from "./routes";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 dotenv.config({ debug: true });
 
 const server = express();
+
+// Apply the rate limiting middleware to all requests
+server.use(limiter);
+server.use(helmet());
 server.use("/", routes);
 
 export const api = functions.https.onRequest(server);
