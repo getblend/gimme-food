b6bc55a1f6730a23f514346548cbe2dd29e68445
kirantheblend <kiran.ml@theblend.co.in>
2022-07-26 13:54:55 +0530

    implement category

---
+++ functions/src/graphql/schema/models/category.ts
@@ -0,0 +1,30 @@
+import { Field, ObjectType } from "type-graphql";
+import { withObjectTracking } from "../mixins";
+import { MenuItem, Store } from "../models"
+
+
+@ObjectType({
+  description: "A details of the category",
+})
+export class Category extends withObjectTracking("Category") {
+  
+  @Field({
+    description: "Code of the category",
+  })
+  public name: string;
+
+  @Field({
+    description: "Description of the category",
+  })
+  public description: string;
+
+  @Field(() => Store, { 
+    description: "Store of the menuitem" 
+  })
+  public store: Store;
+
+  @Field(() => MenuItem, { 
+    description: "Store of the menuitem" 
+  })
+  public menuItem: MenuItem;
+}
